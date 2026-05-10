dd8b5260201b6e69a3c1db0af87bd629f26eaaf4
mxpucci <michelangelopucci.mp@gmail.com>
2026-05-10 12:31:33 +0200

    Revert last 4 commits (Add PEF, Minor fix, CMake fix, Inverted index benchmarks)

--- lib/sux-rs-ffi/target/release/deps/sux_rs_ffi-672dd0fc6b8c396b.d
+++
@@ -1,5 +0,0 @@
-/Users/michelangelopucci/NeaTSLatest/lib/sux-rs-ffi/target/release/deps/sux_rs_ffi-672dd0fc6b8c396b.d: src/lib.rs
-
-/Users/michelangelopucci/NeaTSLatest/lib/sux-rs-ffi/target/release/deps/libsux_rs_ffi-672dd0fc6b8c396b.a: src/lib.rs
-
-src/lib.rs:
--- lib/sux-rs-ffi/target/release/libsux_rs_ffi.d
+++
@@ -1,1 +0,0 @@
-/Users/michelangelopucci/NeaTSLatest/lib/sux-rs-ffi/target/release/libsux_rs_ffi.a: /Users/michelangelopucci/NeaTSLatest/lib/sux-rs-ffi/src/lib.rs
